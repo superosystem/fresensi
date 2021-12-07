176944b70bb298b51afaf511a525610e19f30dff
Syahril <gusrylmubarok@gmail.com>
2021-12-07 08:12:48 +0700

    Project Store

---
+++ prototype/App.php
@@ -0,0 +1,5 @@
+<?php
+
+$store1 = new Store("Toko X", "Kuningan", "Indonesia", "Gadget");
+$store2 = new Store("Toko Y", "Cirebon", "Indonesia", "Gadget");
+$store3 = new Store("Toko Z", "Kuningan", "Indonesia", "Fashion");
