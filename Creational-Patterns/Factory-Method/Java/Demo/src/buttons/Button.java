958e620319e1a704a9d2b7fd5f1c4c8996f3a106
ophthalmologist <gusrylmubarok@gmail.com>
2022-02-21 09:59:44 +0700

    Learn Factory Method

---
+++ Creational-Patterns/Factory-Method/Java/Demo/src/buttons/Button.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package buttons;
+
+/**
+ *
+ * @author Gusryl Mubarok <gusrylmubarok@gmail.com>
+ */
+public interface Button {
+    void render();
+    void onClick();
+}
---
+++ Creational-Patterns/Factory-Method/Java/Demo/src/buttons/HtmlButton.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buttons;
+
+/**
+ *
+ * @author Gusryl Mubarok <gusrylmubarok@gmail.com>
+ */
+public class HtmlButton implements Button {
+    public void render() {
+        System.out.println("<button>Test Button</button>");
+        onClick();
+    }
+
+    public void onClick() {
+        System.out.println("Click! Button says - 'Hello World!'");
+    }
+}
