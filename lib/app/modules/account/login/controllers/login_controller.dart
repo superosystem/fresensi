1c9c65d70931484c21bf75a8744f2d5a10d6cc35
Agus S. Mubarok <gusrylmubarok@gmail.com>
2023-05-02 17:53:59 +0700

    add login

--- lib/app/modules/account/login/controllers/login_controller.dart
+++ lib/app/modules/account/login/controllers/login_controller.dart
@@ -1,23 +1,52 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:fresensi/app/routes/app_pages.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passwordC = TextEditingController();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void login() async {
+    if (emailC.text.isNotEmpty && passwordC.text.isNotEmpty) {
+      try {
+        // signIn with email and password
+        UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: emailC.text,
+          password: passwordC.text,
+        );
+        // check user credential
+        if(userCredential.user != null) {
+          if(userCredential.user!.emailVerified == true) {
+            // Route to home
+            Get.offAllNamed(Routes.HOME);
+          }else{
+            Get.defaultDialog(
+              title: "Account need verification",
+              middleText: "You should verify that email."
+            );
+          }
+        }
+
+      } on FirebaseAuthException catch (err) {
+        if (err.code == 'user-not-found') {
+          if (kDebugMode) {
+            print("No user found for that email.");
+          }
+          Get.snackbar("Problem occurred", "Email is not registered");
+        } else if (err.code == 'wrong-password') {
+          if (kDebugMode) {
+            print("Wrong password provided for that user.");
+          }
+          Get.snackbar(
+              "Problem occurred", "Wrong password provided for that user");
+        }
+      }
+    } else {
+      Get.snackbar("Problem occurred", "All fields can not be empty");
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/account/login/views/login_view.dart
+++ lib/app/modules/account/login/views/login_view.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
   const LoginView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('LoginView'),
+        title: const Text('FRESENSI'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: controller.emailC,
+            decoration: const InputDecoration(
+              labelText: "Email",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            controller: controller.passwordC,
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: "Password",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () {
+                controller.login();
+              },
+              child: const Text("LOGIN"),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text("Forgot Password ?"),
+          ),
+        ],
       ),
     );
   }
