87934015edee4167ebdc126c3f03ba638ae77016
Agus S. Mubarok <gusrylmubarok@gmail.com>
2023-05-04 17:38:17 +0700

    get lat and long on device location

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 Fresensi Mobile Application
 
-## Information
+## Features
+
+- [x] User account management (CRUD)
+  - [x] Account
+  - [x] Employee
+- [x] Presence
+  - [x] Attend
+  - [x] View Presences
+- [x] Geo Location
 
 - Roles [employee or admin]
 - 
--- lib/app/controllers/page_index_controller.dart
+++ lib/app/controllers/page_index_controller.dart
@@ -1,3 +1,5 @@
+import 'package:fresensi/app/widgets/toast_custom.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/foundation.dart';
 import 'package:fresensi/app/routes/app_pages.dart';
 import 'package:get/get.dart';
@@ -9,9 +11,17 @@ class PageIndexController extends GetxController {
     pageIndex.value = index;
     switch (index) {
       case 1:
-        if (kDebugMode) {
-          print("INFO: DO PRESENCE");
+        Map<String, dynamic> resPosition = await _determinePosition();
+        if(resPosition["error"] != true) {
+          Position position = resPosition["position"];
+          ToastCustom.successToast("Success", resPosition["message"]);
+          if (kDebugMode) {
+            print("LAT: ${position.latitude}, LONG: ${position.longitude}");
+          }
+        }else{
+          ToastCustom.errorToast("Problem Occurred", resPosition["message"]);
         }
+
         break;
       case 2:
         Get.offAllNamed(Routes.PROFILE);
@@ -20,5 +30,61 @@ class PageIndexController extends GetxController {
         Get.offAllNamed(Routes.HOME);
         break;
     }
+  }
+
+  // Acquire the current position of the device
+  Future<Map<String, dynamic>> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      // return Future.error('Location services are disabled.');
+      return {
+        "position": null,
+        "message": "Location services are disabled",
+        "error": true,
+      };
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        // return Future.error('Location permissions are denied');
+        return {
+          "position": null,
+          "message": "Location permissions are denied",
+          "error": true,
+        };
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return {
+        "position": null,
+        "message": "Location permissions are permanently denied, we cannot request permissions",
+        "error": true,
+      };
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    Position position = await Geolocator.getCurrentPosition();
+    return {
+      "position": position,
+      "message": "Get the location of your device",
+      "error": false,
+    };
   }
 }
